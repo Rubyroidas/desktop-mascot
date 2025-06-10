24a071e16944a090d832df1970be60a24ab8d4ed
Denis Klyuev <denis.klyuev@gmail.com>
2025-06-10 17:11:03 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+﻿import {app, BrowserWindow} from 'electron';
+import * as path from 'path';
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 300,
+        height: 500,
+        resizable: false,
+        frame: false,       // Borderless window
+        transparent: false, // Set true if you want transparent background
+        webPreferences: {
+            preload: path.join(__dirname, 'renderer.js'),
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+
+    win.loadFile(path.join(__dirname, '../public/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
